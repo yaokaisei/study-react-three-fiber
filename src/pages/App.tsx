eb25bf3c1eaf975bee292bff607caa521f92e1ef
yaokaisei <kaisei.design@gmail.com>
2022-09-18 17:36:08 +0900

    first commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'src/styles/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
